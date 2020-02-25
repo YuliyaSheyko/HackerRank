fe998398f0ed912f9d0cb377902ddf33a248ad92
Yulya <53177275+YuliyaSheyko@users.noreply.github.com>
2020-02-25 12:46:58 +0300

    SuperDigit

---
+++ Solution.scala
@@ -0,0 +1,15 @@
+class Solution {
+  def main(args: Array[String]) {
+    val line = scala.io.StdIn.readLine().split(" ")
+    val n = line(0).toCharArray.map(_.asDigit).sum
+    val k = line(1).toLong
+    val input = (n * k).toString
+    println(input)
+    println(superDigit(input))
+  }
+  @scala.annotation.tailrec
+  final def superDigit(input: String): Int = input.length match {
+    case 1 => input.toInt
+    case _ => superDigit(input.toCharArray.map(_.asDigit).sum.toString)
+  }
+}
